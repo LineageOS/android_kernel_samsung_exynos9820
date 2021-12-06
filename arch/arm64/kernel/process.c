c683976918f8a92530fae7a13ce70fa8c54ec1f7
Tim Zimmermann <tim@linux4.de>
2021-12-06 10:42:24 +0100

    [SQUASH][DNM] Merge branch 'android-4.14-p' of https://android.googlesource.com/kernel/common into lineage-19.0_4.14.256
    Change-Id: I9bfd8902edde6ae62ffe883002490a9394a5b86a

--- arch/arm64/kernel/process.c
+++ arch/arm64/kernel/process.c
@@ -214,7 +214,7 @@ static void show_data(unsigned long addr, int nbytes, const char *name)
 		}
 	}
 
-	printk("\n%s: %#lx:\n", name, addr);
+	printk("\n%s: %pS:\n", name, addr);
 
 	/*
 	 * round address down to a 32 bit boundary
