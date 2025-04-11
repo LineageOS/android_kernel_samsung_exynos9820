3c3320fae5cc4a49e25ad52f0f04b68094ffc508
Tim Zimmermann <tim@linux4.de>
2025-04-11 06:30:12 +0200

    net: wireless: broadcom: bcmdhd_101_16: Don't require userspace to set MFP params
    * After upstream wpa_supplicant commit [1] params.mgmt_frame_protection (which equals sme->mfp here)
      is left set to NO_MGMT_FRAME_PROTECTION causing MFP to never be configured and as a result the network
      speed is very low (< 30 Mbit/s)
    * Broadcom simply removed this check in bcmdhd_101_24 [2]
    
    Ref:
    [1]: https://w1.fi/cgit/hostap/commit/wpa_supplicant/wpa_supplicant.c?id=32dcec9529ec25bc80188b05abf806450d0fde10
    [2]: https://github.com/LineageOS/android_kernel_samsung_gta4xls/blob/lineage-22.2/drivers/net/wireless/broadcom/bcmdhd_101_24/wl_cfg80211.c#L4950
    
    Change-Id: Ia2373b93499bfad58617a181eeb9a0504f9f7e46

--- drivers/net/wireless/broadcom/bcmdhd_101_16/wl_cfg80211.c
+++ drivers/net/wireless/broadcom/bcmdhd_101_16/wl_cfg80211.c
@@ -4711,15 +4711,10 @@ wl_cfg80211_set_mfp(struct bcm_cfg80211 *cfg,
 			(wl_cfg80211_get_rsn_capa(wpa2_ie, &rsn_cap) == 0) && rsn_cap) {
 		WL_DBG(("rsn_cap 0x%x%x\n", rsn_cap[0], rsn_cap[1]));
 		/* Check for MFP cap in the RSN capability field */
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(3, 9, 0))
-		if (sme->mfp)
-#endif
-		{
-			if (rsn_cap[0] & RSN_CAP_MFPR) {
-				mfp = WL_MFP_REQUIRED;
-			} else if (rsn_cap[0] & RSN_CAP_MFPC) {
-				mfp = WL_MFP_CAPABLE;
-			}
+		if (rsn_cap[0] & RSN_CAP_MFPR) {
+			mfp = WL_MFP_REQUIRED;
+		} else if (rsn_cap[0] & RSN_CAP_MFPC) {
+			mfp = WL_MFP_CAPABLE;
 		}
 		/*
 		 * eptr --> end/last byte addr of wpa2_ie
@@ -4728,7 +4723,11 @@ wl_cfg80211_set_mfp(struct bcm_cfg80211 *cfg,
 		eptr = (const u8*)wpa2_ie + (wpa2_ie->len + TLV_HDR_LEN);
 		/* pointing ptr to the next byte after rns_cap */
 		ptr = (const u8*)rsn_cap + RSN_CAP_LEN;
-		if (mfp && (eptr - ptr) >= WPA2_PMKID_COUNT_LEN) {
+		/* If sme->mfp is not set, the bip iovar will not be fired. */
+		if (mfp &&
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(3, 9, 0))
+			(sme->mfp) &&
+#endif
 			/* pmkid now to point to 1st byte addr of pmkid in wpa2_ie */
 			pmkid = (const wpa_pmkid_list_t*)ptr;
 			count = pmkid->count.low | (pmkid->count.high << 8);
