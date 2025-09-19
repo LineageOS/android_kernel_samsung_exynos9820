b11336350ae6da3257bb16419dba0b5ad23a4f23
ExtremeXT <extremextdev@gmail.com>
2025-09-19 16:11:23 +0300

    drivers: media: platform: mfc: switch to __mfc_timeval_compare
    * From exynos2100
    * Required after "UPSTREAM: time: Remove unused functions"
    
    Change-Id: I0c6322663191af6086f48bc7f71c3980521e9b2f
    Signed-off-by: ExtremeXT <extremextdev@gmail.com>

--- drivers/media/platform/exynos/mfc/mfc_qos.h
+++ drivers/media/platform/exynos/mfc/mfc_qos.h
@@ -43,6 +43,15 @@ void mfc_qos_update_framerate(struct mfc_ctx *ctx, u32 bytesused,
 		int idle_trigger_only);
 void mfc_qos_update_last_framerate(struct mfc_ctx *ctx, u64 timestamp);
 
+static inline int __mfc_timeval_compare(const struct timeval *lhs, const struct timeval *rhs)
+{
+	if (lhs->tv_sec < rhs->tv_sec)
+		return -1;
+	if (lhs->tv_sec > rhs->tv_sec)
+		return 1;
+	return lhs->tv_usec - rhs->tv_usec;
+}
+
 static inline void mfc_qos_reset_framerate(struct mfc_ctx *ctx)
 {
 	if (ctx->type == MFCINST_DECODER)
